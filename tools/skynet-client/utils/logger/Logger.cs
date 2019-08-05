327e25b407339713aba9ce9ec8f0de2e5e57e8de
cloudfreexiao <996442717qqcom@gmail.com>
2019-08-05 22:03:06 +0800

    add skyent-client logger module

---
+++ tools/skynet-client/utils/logger/Logger.cs
@@ -0,0 +1,314 @@
+﻿#define UNITY_DIALOGS // Comment out to disable dialogs for fatal errors
+using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR && UNITY_DIALOGS
+using UnityEditor;
+#endif
+
+///////////////////////////
+// Types
+///////////////////////////
+
+[System.Flags]
+public enum Channel : uint
+{
+/// <summary>
+    /// Logs from C# to do with our Lua api
+    /// </summary>
+    Lua             = 1 << 0,
+    /// <summary>
+    /// Logs directly from the Lua VM
+    /// </summary>
+    LuaNative       = 1 << 1,
+    /// <summary>
+    /// Logs to do with AI/GOAP/
+    /// </summary>
+    AI              = 1 << 2,
+    /// <summary>
+    /// Logs to do with graphics/rendering
+    /// </summary>
+    Rendering       = 1 << 3,
+    /// <summary>
+    /// Logs to do with the physics system
+    /// </summary>
+    Physics         = 1 << 4,
+    /// <summary>
+    /// Logs to do with our UI system
+    /// </summary>
+    UI              = 1 << 5,
+    /// <summary>
+    /// Logs about NetDevices and networks
+    /// </summary>
+    NetDevice       = 1 << 6,
+    /// <summary>
+    /// Logs to do with sound and Wwise
+    /// </summary>
+    Audio           = 1 << 7,
+    /// <summary>
+    /// Logs to do with level loading
+    /// </summary>
+    Loading         = 1 << 8,
+    /// <summary>
+    /// Logs to do with localisation
+    /// </summary>
+    Localisation    = 1 << 9,
+    /// <summary>
+    /// Logs to do with platform services
+    /// </summary>
+    Platform        = 1 << 10,
+    /// <summary>
+    /// Logs asserts
+    /// </summary>
+    Assert          = 1 << 11,
+    /// <summary>
+    /// Build/Content generation logs
+    /// </summary>
+    Build           = 1 << 12,
+    /// <summary>
+    /// Analytics logs
+    /// </summary>
+    Analytics       = 1 << 13,
+    /// <summary>
+    /// Animation logs
+    /// </summary>
+    Animation       = 1 << 14,
+}
+
+/// <summary>
+/// The priority of the log
+/// </summary>
+public enum Priority
+{
+    // Default, simple output about game
+    Info,
+    // Warnings that things might not be as expected
+    Warning,
+    // Things have already failed, alert the dev
+    Error,
+    // Things will not recover, bring up pop up dialog
+    FatalError,
+}
+
+public class Logger
+{
+    public const Channel kAllChannels = (Channel) ~0u;
+
+    ///////////////////////////
+    // Singleton set up 
+    ///////////////////////////
+
+    private static Logger _instance;
+    private static Logger Instance => _instance ?? (_instance = new Logger());
+
+    private Logger()
+    {
+        _mChannels 	= kAllChannels;
+    }
+    
+    ///////////////////////////
+    // Members
+    ///////////////////////////
+    private Channel _mChannels;
+
+    public delegate void OnLogFunc(Channel channel, Priority priority, string message);
+    public static event OnLogFunc OnLog;
+
+    ///////////////////////////
+    // Channel Control
+    ///////////////////////////
+
+    public static void ResetChannels()
+    {
+        Instance._mChannels = kAllChannels;
+    }
+
+    public static void AddChannel(Channel channelToAdd)
+    {
+        Instance._mChannels |= channelToAdd;
+    }
+
+    public static void RemoveChannel(Channel channelToRemove)
+    {
+        Instance._mChannels &= ~channelToRemove;
+    }
+
+    public static void ToggleChannel(Channel channelToToggle)
+    {
+        Instance._mChannels ^= channelToToggle;
+    }
+
+    private static bool IsChannelActive(Channel channelToCheck)
+    {
+        return (Instance._mChannels & channelToCheck) == channelToCheck;
+    }
+
+    public static void SetChannels(Channel channelsToSet)
+    {
+        Instance._mChannels = channelsToSet;
+    }
+
+    ///////////////////////////
+
+    ///////////////////////////
+    // Logging functions
+    ///////////////////////////
+
+    /// <summary>
+    /// Standard logging function, priority will default to info level
+    /// </summary>
+    /// <param name="logChannel"></param>
+    /// <param name="message"></param>
+    public static void Log(Channel logChannel, string message)
+    {
+        FinalLog(logChannel, Priority.Info, message);
+    }
+
+    /// <summary>
+    /// Standard logging function with specified priority
+    /// </summary>
+    /// <param name="logChannel"></param>
+    /// <param name="priority"></param>
+    /// <param name="message"></param>
+    public static void Log(Channel logChannel, Priority priority, string message)
+    {
+        FinalLog(logChannel, priority, message);
+    }
+
+    /// <summary>
+    /// Log with format args, priority will default to info level
+    /// </summary>
+    /// <param name="logChannel"></param>
+    /// <param name="message"></param>
+    /// <param name="args"></param>
+    public static void Log(Channel logChannel, string message, params object[] args)
+    {
+        FinalLog(logChannel, Priority.Info, string.Format(message, args));
+    }
+
+    /// <summary>
+    /// Log with format args and specified priority
+    /// </summary>
+    /// <param name="logChannel"></param>
+    /// <param name="priority"></param>
+    /// <param name="message"></param>
+    /// <param name="args"></param>
+    public static void Log(Channel logChannel, Priority priority, string message, params object[] args)
+    {
+        FinalLog(logChannel, priority, string.Format(message, args));
+    }
+
+    /// <summary>
+    /// Assert that the passed in condition is true, otherwise log a fatal error
+    /// </summary>
+    /// <param name="condition">The condition to test</param>
+    /// <param name="message">A user provided message that will be logged</param>
+    public static void Assert(bool condition, string message)
+    {
+        if (!condition)
+        {
+            FinalLog(Channel.Assert, Priority.FatalError, string.Format("Assert Failed: {0}", message));
+        }
+    }
+
+    /// <summary>
+    /// This function controls where the final string goes
+    /// </summary>
+    /// <param name="logChannel"></param>
+    /// <param name="priority"></param>
+    /// <param name="message"></param>
+    private static void FinalLog(Channel logChannel, Priority priority, string message)
+    {
+        if (!IsChannelActive(logChannel)) return;
+        // Dialog boxes can't support rich text mark up, do we won't colour the final string 
+        var finalMessage = ContructFinalString(logChannel, priority, message, (priority != Priority.FatalError));
+
+#if UNITY_EDITOR && UNITY_DIALOGS
+        // Fatal errors will create a pop up when in the editor
+        if (priority == Priority.FatalError)
+        {
+            var ignore = EditorUtility.DisplayDialog("Fatal error", finalMessage, "Ignore", "Break");
+            if (!ignore)
+            {
+                Debug.Break();
+            }
+        }
+#endif 
+        // Call the correct unity logging function depending on the type of error 
+        switch (priority)
+        {
+            case Priority.FatalError:
+            case Priority.Error:
+                Debug.LogError(finalMessage);
+                break;
+
+            case Priority.Warning:
+                Debug.LogWarning(finalMessage);
+                break;
+
+            case Priority.Info:
+                Debug.Log(finalMessage);
+                break;
+        }
+
+        OnLog?.Invoke(logChannel, priority, finalMessage);
+    }
+
+    /// <summary>
+    /// Creates the final string with colouration based on channel and priority 
+    /// </summary>
+    /// <param name="logChannel"></param>
+    /// <param name="priority"></param>
+    /// <param name="message"></param>
+    /// <param name="shouldColour"></param>
+    /// <returns></returns>
+    private static string ContructFinalString(Channel logChannel, Priority priority, string message, bool shouldColour)
+    {
+        string priortiyColour   = PriorityToColour[priority];
+
+        if (channelToColour.TryGetValue(logChannel, out var channelColour))
+            return !shouldColour
+                ? $"[{logChannel}] {message}"
+                : $"<b><color={channelColour}>[{logChannel}] </color></b> <color={priortiyColour}>{message}</color>";
+        channelColour = "black";
+        Debug.LogErrorFormat("Please add colour for channel {0}", logChannel);
+
+        return !shouldColour ? $"[{logChannel}] {message}" : $"<b><color={channelColour}>[{logChannel}] </color></b> <color={priortiyColour}>{message}</color>";
+    }
+
+    /// <summary>
+    /// Map a channel to a colour, using Unity's rich text system
+    /// </summary>
+    private static readonly Dictionary<Channel, string> channelToColour = new Dictionary<Channel, string>
+    {
+        { Channel.Lua,          "cyan" },
+        { Channel.LuaNative,    "lightblue" },
+        { Channel.AI,           "blue" },
+        { Channel.Rendering,    "green" },
+        { Channel.Physics,      "yellow" },
+        { Channel.UI,           "purple" },
+        { Channel.NetDevice,    "orange" },
+        { Channel.Audio,        "teal" },
+        { Channel.Loading,      "olive" },
+        { Channel.Localisation, "brown" },
+        { Channel.Platform,     "red" },
+        { Channel.Assert,       "red" },
+        { Channel.Build,        "navy" },
+        { Channel.Analytics,    "maroon" }
+    };
+
+    /// <summary>
+    /// Map a priority to a colour, using Unity's rich text system
+    /// </summary>
+    private static readonly Dictionary<Priority, string> PriorityToColour = new Dictionary<Priority, string>
+    {
+		#if UNITY_PRO_LICENSE
+        { Priority.Info,        "white" },
+		#else
+		{ Priority.Info,        "black" },
+		#endif
+        { Priority.Warning,     "orange" },
+        { Priority.Error,       "red" },
+        { Priority.FatalError,  "red" },
+    };
+}
